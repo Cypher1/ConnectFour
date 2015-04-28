98c1c0987060066861339a2da90f2fd85f72939b
Cypher <jp10010101010000@gmail.com>
2015-04-28 22:36:52 +1000

    Added some stubs to build on

---
+++ Board.java
@@ -0,0 +1,8 @@
+/**
+An interface for Boards
+
+Allows the game to use different boards
+*/
+public interface Board{
+	
+}
---
+++ BoardRenderer.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+/**
+An interface for BoardRenderers
+
+Allows the interface to be rendered using different rederers
+*/
+public interface BoardRenderer{
+	public void setBoard(Board board);
+	public void renderToFrame();
+	public void setFrame(JFrame window);
+}
