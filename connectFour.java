4a9e6f1bfd954ebbf2efcbf010f60441aac17aa4
Cypher <jp10010101010000@gmail.com>
2015-04-28 22:05:55 +1000

    A stub for the main line

---
+++ connectFour.java
@@ -0,0 +1,19 @@
+/*Main program for COMP2911-Project
+
+	Start Date: 28th April 2015
+	Written By: 
+		Kirsten Hendriks
+		Peng Herrork
+		Lucy Kidd
+		Michelle Phan
+		Joshua Pratt
+*/
+
+public class connectFour{
+
+	public static void main(String args[]){
+		System.out.println("<Stub for the main program>");
+		System.out.println("Should run the game");
+	}
+
+} 
