80953ad2333baff3aec73e787794cf729c95b507
Cypher <jp10010101010000@gmail.com>
2015-04-29 14:03:40 +1000

    Added a stub gui to the main

--- connectFour.java
+++ connectFour.java
@@ -9,11 +9,35 @@
 		Joshua Pratt
 */
 
-public class connectFour{
-
-	public static void main(String args[]){
-		System.out.println("<Stub for the main program>");
-		System.out.println("Should run the game");
-	}
-
-} 
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+ 
+public class connectFour implements Runnable {
+ 
+    @Override
+    public void run() {
+        // Create the window
+        JFrame f = new JFrame("Hello, !");
+        // Sets the behavior for when the window is closed
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Add a layout manager so that the button is not placed on top of the label
+        f.setLayout(new FlowLayout());
+        // Add a label and a button
+        f.add(new JLabel("Hello, world!"));
+        f.add(new JButton("Press me!"));
+        // Arrange the components inside the window
+        f.pack();
+        // By default, the window is not visible. Make it visible.
+        f.setVisible(true);
+    }
+ 
+    public static void main(String[] args) {
+        connectFour gameWindow = new connectFour();
+        // Schedules the application to be run at the correct time in the event queue.
+        SwingUtilities.invokeLater(gameWindow);
+    }
+ 
+}
