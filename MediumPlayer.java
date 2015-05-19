d8a74cdc633d390f03757530afcc51255cecdb6b
mishphan <mishi.me@hotmail.com>
2015-05-19 18:06:46 +1000

    some changes

--- MediumPlayer.java
+++ MediumPlayer.java
@@ -1,62 +1,45 @@
 public class MediumPlayer implements Player {
+
 	public int nextMove (Board current){
-		return minMax(current, 10, 1);
-	}
-	//finds the possible moves that the AI can make
-	//iterate through states and so how many you winning 
-	private int minMax(Board board, int depth, int player) {
-		int bestValue = 0;
-		int val = 0;
-		if (depth == 0 || board.getWinner() != -1) {
-			// return heuristic value
-			return heuristic(board, player);
-		}
-		if(player == 1) { 
-			bestValue = Integer.MIN_VALUE;
-			for(int i = 0; i < board.getWidth(); i++) {
-				Board child = board.clone();
-				if(child.placeMove(i)) {
-					val = -minMax(child, depth - 1, -player);
+		int inf = Integer.MIN_VALUE;
+		//Initial call for Player A's root node
+		int move = 0;
+		double moveVal = inf;
+		for(int op = 0; op < current.getWidth(); op++){
+			Board moveBoard = current.clone();		
+			if(moveBoard.placeMove(op)){
+				double canWin = negaMax(moveBoard, 4, current.getCurrentPlayer());
+					if(canWin > moveVal){
+						moveVal = canWin;
+						move = op;
+					}
 				}
+			}
+		return move;
+	}
+	
+	private double negaMax(Board board, int depth, int player){
+		Integer winner = board.getWinner();//check if the game has ended
+		double bestValue = Integer.MIN_VALUE;
+		if(depth == 0 || winner != -1){
+			if(depth == 0){
+				return 0.1;
+			}
+
+			if(winner == player){
+				return -depth;
+			}
+			return depth;
+		}
+
+		for(int op = 0; op < board.getWidth(); op++){
+			Board childBoard = board.clone();
+			boolean worked = childBoard.placeMove(op);
+			if(worked){//is a legal move
+				double val = 0.3*negaMax(childBoard, depth - 1,  childBoard.getCurrentPlayer());
 				bestValue = Math.max(bestValue, val);
 			}
-			return bestValue;
-		} else {
-			bestValue = Integer.MAX_VALUE;
-			for(int i = 0; i < board.getWidth(); i++) {
-				Board child = board.clone();
-				if(child.placeMove(i)) {
-					val = minMax(child, depth - 1, player);
-				}
-				bestValue = Math.min(bestValue, val);
-			}
-			return bestValue;
 		}
-	}
-
-	private int heuristic (Board board, int player) {
-		int heuristicValue = 0;
-		// analyse current board state
-		// give it a value to determine benefit to player
-
-		for(int i = 0; i < board.getWidth(); i++) {
-			int consecutive = 0;
-			for(int j = 0; j < board.getHeight(); j++) {
-				if(board.getState(i, j) == player) {
-					consecutive++;
-				} else {
-					consecutive = 0;
-				}
-				if(consecutive == 3) {
-					heuristicValue += 100;
-				}
-				if(consecutive == 4) {
-					heuristicValue += 500;
-				}
-			}
-		}
-
-
-		return heuristicValue;
+		return -bestValue;
 	}
 }
