b10efed3ae44077901dcd7fffb774645eb7b3377
Kirsten Hendriks <kirsten.e.m.hendriks@gmail.com>
2015-05-26 20:33:45 +1000

    Added Javadoccing for the Player files

--- EasyPlayer.java
+++ EasyPlayer.java
@@ -1,12 +1,28 @@
 import java.util.Random;
-
+/**
+* An easy player which simply drops it's token in a random column
+*/
 public class EasyPlayer implements Player {
 	Random generator;
 
+	/**
+	* A constructor which creates a Random class
+	*/
 	public EasyPlayer() {
 		generator = new Random();
 	}
 
+	/**
+	* Given a current board this returns the column in which the player would like to place
+	* it's next move. Easy player just generates a random column index within the bounds of 
+	* the board.<p>
+	* pre: the board is valid<br>
+	* post: the overall game state is not changed, but a value where a move is to be placed
+	* is returned 
+	* @param current: the current state of the board, not actually used by this player but
+	* is required to implement Player
+	* @return the column index which the move should be played in
+	*/
 	public int nextMove (Board current){
 		return generator.nextInt(current.getWidth()-1);
 	}
--- HumanPlayer.java
+++ HumanPlayer.java
@@ -1,5 +1,6 @@
 /**
-	A human player, this acts as an interface between the simulator and the GUI
+* A class that implements Player, but is controlled by the human. This acts as an interface
+* between simulator and the humans
 */
 public class HumanPlayer implements Player {
 
@@ -7,16 +8,18 @@ public class HumanPlayer implements Player {
 	private int nextMove;
 
 	/**
-		constructor initialises the human player to no moves made. 
+	* A constructor for human player. This sets the next move as no move being made. 
 	*/
 	public HumanPlayer() {
 		this.nextMove = NO_MOVE;
 	}
 
 	/**
-		The function that will get called when a move should be made
-		@params an integer between 0 and 6 that represents the column 
-			of the next move
+	* The function that will get called by the UI when a move has been made by the human<p>
+	* pre: the given column is within the range of the current board<br>
+	* post: nextMove has been set as the given input within the humanplayer class
+	* @param column: an integer between 0 and the board width that represents the column of
+	* the next move
 	*/
 	public void makeMove(int column)
 	{
@@ -24,18 +27,24 @@ public class HumanPlayer implements Player {
 	}
 
 	/**
-		The function responsible for checking if a move has been made. 
-		@params the current board
-		@return -1 if no move has been made yet or the number of the 
-			column of the move
+	* The function responsible for checking if a move has been made, this will be called
+	* by the simulator. <p>
+	* pre: none <br>
+	* post: either the new move has been returned and nextMove has been set back to NO_MOVE,
+	* or NO_MOVE has been returned and no change is made to nextMove
+	* @param current: the current board, this is required to conform the HumanPlayer to the
+	* Player interface but is not actually used.
+	* @return -1 if no move has been made yet or the number of the column of the move
 	*/
 	public int nextMove (Board current){
-		int move = -1;
+		int move = NO_MOVE;
 		//check move has been made and reinitialise
-		if(this.nextMove != NO_MOVE)
+		if(this.nextMove != NO_MOVE) {
 			move = this.nextMove;
-
-		this.nextMove = NO_MOVE;
+			//reset the nextMove to no move so that the same move does not get played twice
+			this.nextMove = NO_MOVE;
+		} 
+		
 		return move;
 	}
 }
